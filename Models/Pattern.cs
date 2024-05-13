774fdca0dbe0f5172ba90d9c5eb5e161521a9356
Peaceteddie <Peaceteddie@gmail.com>
2024-05-13 19:42:51 +0200

    chore: Refactor Pattern.cs and Home.razor, add StitchViewer and StitchEditor components

--- Models/Pattern.cs
+++ Models/Pattern.cs
@@ -5,7 +5,7 @@ namespace InCaseIForgetMyCrochet.Models;
 public class Pattern
 {
     [Key] public int Id { get; set; }
-    [Required] public required string Name { get; set; } = "";
+    [Required] public string Name { get; set; } = "";
     public List<Row> Rows { get; set; } = [];
 }
 
