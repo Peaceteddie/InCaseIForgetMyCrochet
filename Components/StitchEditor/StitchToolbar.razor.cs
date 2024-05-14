e09f0c87224c97717f16e5c2dc1be09c6a86f431
Peaceteddie <Peaceteddie@gmail.com>
2024-05-15 01:47:31 +0200

    fix: Reconnected toolbar and viewer

---
+++ Components/StitchEditor/StitchToolbar.razor.cs
@@ -0,0 +1,33 @@
+﻿using InCaseIForgetMyCrochet.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace InCaseIForgetMyCrochet.Components.StitchEditor;
+
+public partial class StitchToolbar
+{
+    [Parameter] public Pattern? Pattern { get; set; }
+    [Parameter] public EventCallback Refresh { get; set; }
+    [Parameter] public EventCallback<int> StitchAmountChanged { get; set; }
+    [Parameter] public EventCallback<StitchTypeAbbreviation> StitchTypeChanged { get; set; }
+
+    int _StitchAmount;
+    int StitchAmount
+    {
+        get => _StitchAmount;
+        set
+        {
+            _StitchAmount = value;
+            StitchAmountChanged.InvokeAsync(value);
+        }
+    }
+    StitchTypeAbbreviation _StitchType;
+    StitchTypeAbbreviation StitchType
+    {
+        get => _StitchType;
+        set
+        {
+            _StitchType = value;
+            StitchTypeChanged.InvokeAsync(value);
+        }
+    }
+}
---
+++ Components/StitchEditor/StitchViewer.razor.cs
@@ -0,0 +1,13 @@
+﻿using InCaseIForgetMyCrochet.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace InCaseIForgetMyCrochet.Components.StitchEditor;
+public partial class StitchViewer
+{
+    [Parameter] public Pattern? Pattern { get; set; }
+
+    [Parameter] public int StitchAmount { private get; set; }
+
+    [Parameter] public StitchTypeAbbreviation StitchType { private get; set; }
+
+}
